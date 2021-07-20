02e3a902ad7f6479345d79c03e9359cfd7c24c94
Reid <Reid.Finn@yahoo.com>
2021-07-20 12:45:10 -0500

    setting up homepage

---
+++ src/Components/HomePage.js
@@ -0,0 +1,16 @@
+import BeartreePic from './Bearpawlogo-mine.pdf';
+
+
+
+function HomePage() {
+    return (
+        <div>
+            <h1>BEARTREE</h1>
+            <img src={BeartreePic} alt=''>
+            </img>
+        </div>
+    )
+}
+
+
+export default HomePage
